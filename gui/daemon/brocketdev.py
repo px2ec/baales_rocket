e84dd26f658e52bccdc32b911d7f60b47fe26154
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2016-07-04 06:10:28 -0400

    Updates

--- gui/daemon/brocketdev.py
+++ gui/daemon/brocketdev.py
@@ -8,20 +8,22 @@ class bRocketDev(moddev.ModDev):
 	
 	# "Public" variables for class
 	ErrorFlag = False # Error flag for any device error
+	yaw = 0;
+	pitch = 0;
+	roll = 0;
+	sealevelPressure = 101325
+	fst_refresh = False
 
-	# acX = 0
-	# acY = 0
-	# acZ = 0
-	tX = 0
-	tY = 0
-	abstX = 0
-	abstY = 0
-	buttonI = 0
-	buttonD = 0
-	buttonTgl = 0
+	pressure = 0;
+	_p0 = 0;
 
 	def __init__(self):
 		super(bRocketDev, self).__init__()
+
+	def getAltitude(self, pressure_offset = sealevelPressure):
+		if not pressure_offset:
+			pressure_offset = self.sealevelPressure
+		return 44330 * (1.0 - pow(float(self.pressure) / float(pressure_offset),1./5.255));
 
 	def getState(self):
 		lst = [0xFF, CHECK_STATE, 1, CHECK_STATE]
@@ -31,39 +33,29 @@ class bRocketDev(moddev.ModDev):
 			if lread[1] != CHECK_STATE :
 				self.ErrorFlag = True
 				return
-			# self.acX = struct.unpack( "h", chr(lread[4]) + chr(lread[3]))[0]
-			# self.acY = struct.unpack( "h", chr(lread[6]) + chr(lread[5]))[0]
-			# self.acZ = struct.unpack( "h", chr(lread[8]) + chr(lread[7]))[0]
-			# if (lread[4] >> 7) == 1:
-			# 	self.acX = (((~lread[4] & 0x7F) << 8) | ~lread[3])
-			# else:
-			# 	self.acX = ((lread[4] & 0x7F) << 8) | lread[3]
-			# if (lread[6] >> 7) == 1:
-			# 	self.acY = (((~lread[6] & 0x7F) << 8) | ~lread[5])
-			# else:
-			# 	self.acY = ((lread[6] & 0x7F) << 8) | lread[5]
-			# if (lread[8] >> 7) == 1:
-			# 	self.acZ = (((~lread[8] & 0x7F) << 8) | ~lread[7])
-			# else:
-			# 	self.acZ = ((lread[8] & 0x7F) << 8) | lread[7]
-			# self.buttonI = lread[10]
-			# self.buttonD = lread[9]
-			# self.buttonTgl = lread[11]
-			sign = lread[3]
-			#print lread[4], lread[5]
-			self.abstX = lread[4]
-			self.abstY = lread[5]
-			value = sign
-			result = {
-			0: (lread[4], lread[5]),
-			1: (-lread[4], lread[5]),
-			2: (lread[4], -lread[5]),
-			3: (-lread[4], -lread[5])
-			}
-			self.tX, self.tY = result[sign]
-			#print self.tX, self.tY
-			self.buttonI = lread[7]
-			self.buttonD = lread[6]
-			self.buttonTgl = lread[8]
+			yawt = lread[4] + (lread[3] / 100.)
+			if lread[5]:
+				self.yaw = -yawt
+			else:
+				self.yaw = yawt
+
+			pitcht = lread[7] + (lread[6] / 100.)
+			if lread[8]:
+				self.pitch = -pitcht
+			else:
+				self.pitch = pitcht
+			
+			rollt = lread[10] + (lread[9] / 100.)
+			if lread[11]:
+				self.roll = -rollt
+			else:
+				self.roll = rollt;
+
+			#self.pressure = (lread[12] + lread[13] * 256) * 8 + lread[14]
+			self.pressure = lread[12] + ((lread[13]) * 256) + (lread[14] * (2 ** 16)) + (lread[15] * (2 ** 24))
+			if not self.fst_refresh:
+				self._p0 = self.pressure
+				self.fst_refresh = True
+
 			return
 		self.ErrorFlag = True
